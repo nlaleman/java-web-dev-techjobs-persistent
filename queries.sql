fc3860cdb7946d187f4d45a0eed3864eebc58092
nlaleman <dragonmyace@gmail.com>
2021-07-08 15:30:10 -0500

    Done Part 1

--- queries.sql
+++ queries.sql
@@ -1,5 +1,12 @@
 ## Part 1: Test it with SQL
 
+id INT
+employer VARCHAR(255)
+name VARCHAR(255)
+skills VARCHAR(255)
+
+(I also have a hibernate_sequence table with next_val BIGINT)
+
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
